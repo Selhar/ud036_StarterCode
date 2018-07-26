cf0d47c14385738f85e6004bf59e12eb99f589c4
selhar <selhar@protonmail.com>
2018-07-26 12:13:01 -0300

    Correções

--- media.py
+++ media.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
-# Código especificando que este arquivo utiliza UTF-8 e não ascii
-# necessário para usar acentuação: https://www.python.org/dev/peps/pep-0263/
-import webbrowser
+
 
 class Movie():
     "Classe para definição da estrutura de um filme"
-    
+
     def __init__(self, movie_title, poster_image_url, trailer_youtube_url):
         self.title = movie_title
         self.poster_image_url = poster_image_url
